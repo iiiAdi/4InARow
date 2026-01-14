72e02fc466a96cfbd9557c95167d2d3409d63c5a
iiiAdi <adimak112233@gmail.com>
2026-01-14 11:21:52 +0200

    Removed Board Commands from main

--- Main.c
+++ Main.c
@@ -2,57 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "MainFunctions.h"
-#include "Colors.h"
 
-#define ROWS 6
-#define COLS 7
-
-
-void CleanConsole(){
-	system("cls");
-}
 
 void LoadMenu() {
 	printf("Welcome to 4 in A Row!\n");
 	printf("1. PvP\n");
 	printf("2. PvE\n");
 	printf("3. Exit\n");
-}
-
-void printBoard(int gameArr[][COLS]) {
-	CleanConsole();
-	int i, j, numberInPos;
-	
-	// Prints the current game Matrix!
-	for (i = 0; i < ROWS; i++) {
-		for (j = 0; j < COLS; j++) {
-			numberInPos = gameArr[i][j];
-			if (numberInPos > 0) {
-				if (numberInPos == 1) {
-					printf(YELLOW);
-				}
-				else
-					printf(RED);
-				
-				printf("%2c" RESET, 'O');
-				continue;
-			}
-			printf("%2c", 'X');
-		}
-		printf("\n");
-	}
-
-	// Prints the game Numbers:
-
-	for (i = 0; i < COLS * 2; i++) {
-		printf("-");
-	}
-
-	printf("\n");
-	for (i = 0; i < COLS; i++) {
-		printf("%2d", i + 1);
-	}
-	printf("\n");
 }
 
 int countDirection(int gameArr[][COLS], int row, int col, int dRow, int dCol, int player) {
@@ -109,15 +65,7 @@ int PlaceDisc(int gameArr[][COLS], int colNumber, int Player, int* Winner) {
 	return 0;
 }
 
-void ResetBoard(int gameArr[][COLS]) {
-	int i, j;
-	for (i = 0; i < ROWS; i++) {
-		for (j = 0; j < COLS; j++) {
-			gameArr[i][j] = 0;
-		}
-	}
-	printBoard(gameArr);
-}
+
 
 void PlayerVsPlayer() {
 	int playerTurn = 1, Winner = 0, Selection, isValid;
