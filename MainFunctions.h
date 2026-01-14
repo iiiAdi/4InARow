d7d2aaf0d26f612fd059226b54963a25d69e9e55
iiiAdi <adimak112233@gmail.com>
2026-01-14 11:22:16 +0200

    Added definitions for the functions, organized

--- MainFunctions.h
+++ MainFunctions.h
@@ -1,4 +1,14 @@
 #pragma once
 #include <stdio.h>
 
+// Defintion of Rows and Coloumns
+#define ROWS 6
+#define COLS 7
+
+// Main User Functions
 void getInputInt(int, int, int*);
+
+
+// Game-Board Main Functions
+void ResetBoard(int gameArr[][COLS]);
+void printBoard(int gameArr[][COLS]);
